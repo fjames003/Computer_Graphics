21b5ba39ae837884d4f34c399f25f7414e967f1b
Frankie <fjames1@lion.lmu.edu>
2016-01-20 11:02:25 -0800

    Files for canvas template (also found in bazaar)

---
+++ canvas_template.js
@@ -0,0 +1,34 @@
+/*
+ * This template file is meant to be a template for canvas-based
+ * web page code.  Nothing here is set in stone; it is mainly
+ * intended to save you some typing.
+ */
+// Yes, we can use jQuery here, but avoid it just in case you
+// really don't want to use it.  We do still keep things away
+// from the global namespace.
+(function () {
+    // Ditto on using jQuery here.
+    var canvas = document.getElementById("canvas");
+    var renderingContext = canvas.getContext("2d");
+
+    // Declare other variables here.
+    var radialGradient = renderingContext.createRadialGradient(160, 160, 1, 180, 180, 320);
+
+    // Put your canvas drawing code (and any other code) here.
+    radialGradient.addColorStop(0, "white");
+    radialGradient.addColorStop(0.333, "red");
+    radialGradient.addColorStop(1, "blue");
+
+    renderingContext.fillStyle = radialGradient;
+    renderingContext.beginPath();
+    renderingContext.arc(256, 256, 200, 0, Math.PI * 2, true);
+    renderingContext.fill();
+
+    renderingContext.fillStyle = "black";
+    renderingContext.fillRect(300, 256, 500, 75);
+
+    renderingContext.fillStyle = "rgb(34, 200, 90)";
+    renderingContext.beginPath();
+    renderingContext.arc(700, 300, 100, 0, Math.PI / 4, true);
+    renderingContext.fill();
+}());
