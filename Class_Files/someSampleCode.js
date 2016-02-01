255f6b58b5f7887ccb824c9fb5b39ab27fa12dff
Frankie James <fjames1@lion.lmu.edu>
2016-02-01 11:31:48 -0800

    Added my trapazoid code that I ended up not using, still useful though.

---
+++ Class_Files/someSampleCode.js
@@ -0,0 +1,26 @@
+    // This is to make an attempt at a more "woody" look... Doesn't even get close
+    // var radialGradient = renderingContext.createRadialGradient(0, 0, 500, 0, 0, 1000);
+    // radialGradient.addColorStop(0, "rgb(150, 82, 1)");
+    // radialGradient.addColorStop(1, "rgb(192, 155, 76)");
+    // renderingContext.fillStyle = radialGradient;
+
+    // renderingContext.globalAlpha = 0.2;
+    // for (i=0;i<15;i++){
+    //     renderingContext.beginPath();
+    //     renderingContext.arc(75,75,10+10*i,0,Math.PI*2,true);
+    //     renderingContext.fill();
+    // }
+
+
+    // defineTrapezoid(100, 100, 125, 100, 75, "black");
+    // function defineTrapezoid(x, y, bottomSize, topSize, height, color) {
+    //     var difference = (topSize - bottomSize) / 2;
+    //     renderingContext.fillStyle = color;
+    //     renderingContext.beginPath();
+    //     renderingContext.moveTo(x, y);
+    //     renderingContext.lineTo(x + topSize, y);
+    //     renderingContext.lineTo(x + bottomSize + difference, y + height);
+    //     renderingContext.lineTo(x + difference, y + height);
+    //     renderingContext.closePath();
+    //     renderingContext.fill();
+    // }
