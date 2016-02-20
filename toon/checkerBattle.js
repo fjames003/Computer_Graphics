7cc7fac5c23586e02a6231513c61418a16090883
Frankie James <fjames1@lion.lmu.edu>
2016-02-20 14:51:44 -0800

    Overhauled script... Removed indivdual draw functions and instead inline coded them. I also added a function to update parameters and started animating scene...

--- toon/checkerBattle.js
+++ toon/checkerBattle.js
@@ -3,13 +3,15 @@
  * engine is used.
  */
 (function () {
+
     var getTileLocations = function(tileNumber) {
         var tileSize = 122;
             var tileXLocation = (tileNumber % 8) ? (tileNumber % 8) - 1 : 7;
             var tileYLocation = Math.ceil(tileNumber / 8) - 1;
             tileOne = (12 + tileSize / 2);
             return [tileOne + (tileSize * tileXLocation), tileOne + (tileSize * tileYLocation)];
-        };
+    };
+
     var canvas = document.getElementById("canvas");
     window.SpriteLibrary = window.SpriteLibrary || { };
     // First, a selection of "drawing functions" from which we
@@ -18,67 +20,69 @@
     var renderingContext = canvas.getContext("2d");
     renderingContext.save();
     renderingContext.scale(0.7, 0.7);
-    var board = function (renderingContext) {
-        SpriteLibrary.drawBoard({
-          renderingContext: renderingContext, 
-          border: 12,
-          degradation: 0,
-          waveFrequency: 0.03
-        });
 
-    };
-
-    var blackPiece = function (renderingContext) {
-        SpriteLibrary.drawPiece({
-                renderingContext: renderingContext,
-                color: "black",
-                limbAngle: 120,
-                facialExpression: -1,
-                elbowAngle: 135
-            });
-    };
-
-    var redPiece = function (renderingContext) {
-        SpriteLibrary.drawPiece({
-                renderingContext: renderingContext,
-                color: "red",
-                limbAngle: 95,
-                facialExpression: 0,
-                elbowAngle: -135
-            });
-    };
-
-    // Then, we have "easing functions" that determine how
-    // intermediate frames are computed.
-
-    // Now, to actually define the animated sprites.  Each sprite
-    // has a drawing function and an array of keyframes.
     var sprites = [
         {
-            draw: board,
-            keyframes: [
-                {
-                    frame: 0,
-                    ease: KeyframeTweener.linear
-                },
+        wavesDefault: 0.03,
+        degradationDefault: 0,
+        draw: function (renderingContext) {
+            SpriteLibrary.drawBoard({
+                renderingContext: renderingContext, 
+                border: 12,
+                degradation: this.degradationDefault,
+                waveFrequency: this.wavesDefault
+            })
+        },
+        parameterize: function(ease, startKeyFrame, endKeyframe, currentTweenFrame, duration) {
+            this.wavesDefault = parameterUpdate(this.wavesDefault, "waveFrequency", ease, startKeyFrame, endKeyframe, currentTweenFrame, duration);
+            this.degradationDefault = parameterUpdate(this.degradationDefault, "degradation", ease, startKeyFrame, endKeyframe, currentTweenFrame, duration);
+        },
+        keyframes: [
+            {
+                frame: 0,
+                ease: KeyframeTweener.linear,
+                waveFrequency: 0.03,
+                degradation: 0.000001
+            },
 
-                {
-                    frame: 200
-                }
-            ]
+            {
+                frame: 200,
+                waveFrequency: 0.1,
+                degradation: 100
+            }
+        ]
         },
 
         {
-            draw: blackPiece,
+            limbAngleDefault: 90,
+            facialExpressionDefault: 5,
+            elbowAngleDefault: 90,
+            draw: function (renderingContext) {
+                renderingContext.save();
+                renderingContext.scale(0.48, 0.48);
+                SpriteLibrary.drawPiece({
+                    renderingContext: renderingContext,
+                    color: "black",
+                    limbAngle: this.limbAngleDefault,
+                    facialExpression: this.facialExpressionDefault,
+                    elbowAngle: this.elbowAngleDefault
+                })
+                renderingContext.restore();
+            },
+            parameterize: function(ease, startKeyFrame, endKeyframe, currentTweenFrame, duration) {
+                this.limbAngleDefault = parameterUpdate(this.limbAngleDefault, "limbAngle", ease, startKeyFrame, endKeyframe, currentTweenFrame, duration);
+
+                this.elbowAngleDefault = parameterUpdate(this.elbowAngleDefault, "elbowAngle", ease, startKeyFrame, endKeyframe, currentTweenFrame, duration);
+
+                this.facialExpressionDefault = parameterUpdate(this.facialExpressionDefault, "facialExpression", ease, startKeyFrame, endKeyframe, currentTweenFrame, duration);
+            },
             keyframes: [
                 {
                     frame: 0,
                     tx: getTileLocations(1)[0],
                     ty: getTileLocations(1)[1],
-                    // tx: 73,
-                    // ty: 73,
-                    sx: 0.45,
-                    sy: 0.45,
+                    limbAngle: 50,
+                    elbowAngle: 0,
                     ease: KeyframeTweener.quadEaseOut
                 },
 
@@ -86,10 +90,8 @@
                     frame: 100,
                     tx: getTileLocations(64)[0],
                     ty: getTileLocations(64)[1],
-                    // tx: 927,
-                    // ty: 927,
-                    sx: 0.45,
-                    sy: 0.45,
+                    limbAngle: 190,
+                    elbowAngle: 360,
                     ease: KeyframeTweener.quadEaseOut
                 },
 
@@ -97,36 +99,47 @@
                     frame: 150,
                     tx: getTileLocations(32)[0],
                     ty: getTileLocations(32)[1],
-                    // tx: 927,
-                    // ty: 439,
-                    sx: 0.45,
-                    sy: 0.45
+                    limbAngle: 90,
+                    elbowAngle: 90
                 }
             ]
         },
 
         {
-            draw: redPiece,
+            limbAngleDefault: 90,
+            facialExpressionDefault: 5,
+            elbowAngleDefault: 90,
+            draw: function (renderingContext) {
+                renderingContext.save();
+                renderingContext.scale(0.48, 0.48);
+                SpriteLibrary.drawPiece({
+                    renderingContext: renderingContext,
+                    color: "red",
+                    limbAngle: this.limbAngleDefault,
+                    facialExpression: this.facialExpressionDefault,
+                    elbowAngle: this.elbowAngleDefault
+                })
+                renderingContext.restore();
+            },
+            parameterize: function(ease, startKeyFrame, endKeyframe, currentTweenFrame, duration) {
+                this.limbAngleDefault = parameterUpdate(this.limbAngleDefault, "limbAngle", ease, startKeyFrame, endKeyframe, currentTweenFrame, duration);
+
+                this.elbowAngleDefault = parameterUpdate(this.elbowAngleDefault, "elbowAngle", ease, startKeyFrame, endKeyframe, currentTweenFrame, duration);
+
+                this.facialExpressionDefault = parameterUpdate(this.facialExpressionDefault, "facialExpression", ease, startKeyFrame, endKeyframe, currentTweenFrame, duration);
+            },
             keyframes: [
                 {
                     frame: 0,
                     tx: getTileLocations(57)[0],
                     ty: getTileLocations(57)[1],
-                    // tx: 73,
-                    // ty: 927,
-                    sx: 0.45,
-                    sy: 0.45,
                     ease: KeyframeTweener.quadEaseOut
                 },
 
                 {
                     frame: 125,
                     tx: getTileLocations(8)[0],
-                    ty: getTileLocations(8)[1],
-                    // tx: 927,
-                    // ty: 73,
-                    sx: 0.45,
-                    sy: 0.45
+                    ty: getTileLocations(8)[1]
                 }
             ]
         }
@@ -141,4 +154,13 @@
         height: canvas.height,
         sprites: sprites
     });
+    var parameterUpdate = function(propDefault, property, ease, startKeyFrame, endKeyframe, currentTweenFrame, duration) {
+                var start_property = startKeyFrame[property] || propDefault
+                var property_distance = (endKeyframe[property] || propDefault) - start_property;
+                propDefault = ease(currentTweenFrame, start_property, property_distance, duration);
+                return propDefault;
+    };
 }());
+
+
+
