e0dad4f162e52ff43f876925287fb28621cbc829
Frankie James <fjames1@lion.lmu.edu>
2016-03-06 20:30:22 -0800

    Added code from my keyframe copier... Who knows, could be useful later...

--- Class_Files/someSampleCode.js
+++ Class_Files/someSampleCode.js
@@ -24,3 +24,38 @@
     //     renderingContext.closePath();
     //     renderingContext.fill();
     // }
+
+
+    // var keyframeCopier = function(sprite, oldFrame, newFrame, updates) {
+    //     var previousKeyframeLength = sprites[sprite].keyframes.length;
+    //     for (var keyframe = 0; keyframe < previousKeyframeLength; keyframe++) {
+    //         if (sprites[sprite].keyframes[keyframe].frame === oldFrame) {
+
+    //             sprites[sprite].keyframes[previousKeyframeLength] = {};
+    //             for (var property in sprites[sprite].keyframes[keyframe]) {
+    //                 if (sprites[sprite].keyframes[keyframe].hasOwnProperty(property)) {
+    //                     sprites[sprite].keyframes[previousKeyframeLength][property] = 
+    //                     sprites[sprite].keyframes[keyframe][property];
+    //                 }
+    //             }
+
+    //             console.log(sprites[sprite].keyframes[previousKeyframeLength]);
+    //             sprites[sprite].keyframes[previousKeyframeLength].frame = newFrame;
+    //             for (var update in updates) {
+    //                 if (update === "parameters") {
+    //                     for (var paramUpdate in updates[update]) {
+    //                         console.log(sprites[sprite].keyframes[previousKeyframeLength][update]);
+    //                         console.log(!sprites[sprite].keyframes[previousKeyframeLength][update]);
+    //                         if (!sprites[sprite].keyframes[previousKeyframeLength][update]) {
+    //                             sprites[sprite].keyframes[previousKeyframeLength][update] = {};
+    //                         }
+    //                         sprites[sprite].keyframes[previousKeyframeLength][update][paramUpdate] = updates[update][paramUpdate];
+    //                     }
+    //                 } else {
+    //                 sprites[sprite].keyframes[previousKeyframeLength][update] = updates[update];                       
+    //                 }
+    //             }
+    //             console.log(sprites[sprite].keyframes[previousKeyframeLength]);
+    //         }
+    //     }
+    // }
