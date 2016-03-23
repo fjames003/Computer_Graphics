80778182ce073523251c9bee31313b7a29f96465
Frankie <fjames1@lion.lmu.edu>
2016-03-23 12:29:20 -0700

    Fixed some typos, converted the 'equal' that I was using to test arrays to 'deepEqual' as this now works. I have all passing tests now for a default identity matrix as well as a basic user friendly matrix. Should now handle non-user friendly input...

--- matrix/matrix-test.js
+++ matrix/matrix-test.js
@@ -5,28 +5,36 @@ $(function () {
 
     // This suite checks instantiation basics.
     test("Creation and Data Access", function () {
-
+        var count;
         var mat = new Matrix([[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]);
 
         equal(mat.rowDimensions(), 4, "Matrix row size");
         equal(mat.colDimensions(), 4, "Matrix column size");
-        equal(mat.size(), [4,4], "Matrix size");
-        for (var i = 0; i < 16; i += 1) {
-            equal(mat.elements[i], i + 1, "Element " + (i + 1) + " by index");
+        deepEqual(mat.size(), [4,4], "Matrix size");
+        count = 1;
+        for (var i = 0; i < 4; i += 1) {
+            for (var j = 0; j < 4; j += 1) {
+                equal(mat.elements[i][j], count, "Element " + count + " by index");
+                count += 1;
+            }
         };
 
         // By default the matrix object should intialize to the identity matrix
-        mat = new Mector();
+        mat = new Matrix();
 
         equal(mat.rowDimensions(), 4, "Matrix row size");
         equal(mat.colDimensions(), 4, "Matrix column size");
-        equal(mat.size(), [4,4], "Matrix size");
-        for (var i = 0; i < 16; i += 1) {
-            if (i === 0 || i === 5 || i === 10 || i === 15) {
-                equal(mat.elements(i), 1, "Diagonal of ones in identity matrix");
-            } else {
-                equal(mat.elements(i), 0, "Non-diagonal elements of identity matrix");
+        deepEqual(mat.size(), [4,4], "Matrix size");
+        for (var i = 0; i < 4; i += 1) {
+            equal(mat.elements[i][i], 1, "Diagonals of identity matrix should be 1")
+        };
+        for (var i = 0; i < 4; i += 1) {
+            for (var j = 0; j < 4; j += 1) {
+                if (i !== j) {
+                    equal(mat.elements[i][j], 0, "Non-Diagonal elements should be 0");
+                }
             }
         };
+
     });
 });
