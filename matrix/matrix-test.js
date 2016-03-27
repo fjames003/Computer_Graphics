0ba931bcb24b61585a4ede56b04d829d5d617c85
Frankie <fjames1@lion.lmu.edu>
2016-03-27 13:58:16 -0700

    Added in a rotation matrix test... This one is only about the y axis...

--- matrix/matrix-test.js
+++ matrix/matrix-test.js
@@ -104,6 +104,18 @@ $(function () {
                                                                          [0, 0, -1, 0]]), "Creating a 3D orthographic projection matrix");
     });
 
+    test("3D Rotation matrix", function () {
+        var mat1 = new Matrix();
+        var tester = mat1.rotation(90, 0.0, 1.0, 0.0);
+        var yRot = new Matrix([[0, 0, 1, 0],
+                               [0, 1, 0, 0],
+                               [-1, 0, 0, 0],
+                               [0, 0, 0, 1]]);
+        tester.forEach(function(value, index, matrix) {
+            QUnit.close(value, yRot.elements[index[0]][index[1]], 0.00001, "Creating a 3D rotation matrix about the y axis");
+        })
+    });
+
     test("Conversion to WebGL and GLSL format", function () {
         var mat1 = new Matrix([[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]);
         deepEqual(mat1.toWebGL(), new Float32Array([1,5,9,13,2,6,10,14,3,7,11,15,4,8,12,16]), "Converting a matrix to WebGL format");
