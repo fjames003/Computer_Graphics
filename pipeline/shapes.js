da2030bdcf05b84afc6248476f756a9b9006b59d
Frankie <fjames1@lion.lmu.edu>
2016-03-28 11:20:29 -0700

    YEEEEESSS, I finally got the OOP aspect of Javascript to work for me... I Eliminated the repeat code from sphere and also turned shapeLibrary into a bonafide library of objects... (I think). Now time to add some more shapes... And add some tests for them...

--- pipeline/shapes.js
+++ pipeline/shapes.js
@@ -53,53 +53,33 @@ var Shape = (function () {
     return shape;
 })();
 
-var ShapeLibrary = (function () {
-    var fillColors = function (number, r, g, b) {
-        var result = [];
-        for (var i = 0; i < number; i += 1) {
-            result = result.concat(
-                r,
-                g,
-                b
-            );
-        }
-        return result;
-    };
-    var sphereColors = {r: 0.0, g: 0.0, b: 0.0};
-    var vertices = [];
-    var sphereMode;
-    var sphere = function (n, colors, mode) {
-        
-        this.mode = mode;
-        vertices = [];
-        for (var i = 0.0; i < n; i += 1) {
-            for (var j = 0.0; j < n - 1; j += 1) {
-                vertices = vertices.concat(
-                    Math.sin(Math.PI * i/n) * Math.cos(2 * Math.PI * j/n),
-                    Math.sin(Math.PI * i/n) * Math.sin(2 * Math.PI * j/n), 
-                    Math.cos(Math.PI * i/n)
-                );
-            }
-        }
-        this.vertices = vertices;
-        if (colors.r || colors.g || colors.b) {
-            colors = colors || {r: 0.0, g: 0.0, b: 0.0};
-            this.colors = [].concat(fillColors(vertices.length / 3, colors.r, colors.g, colors.b));
-        } else {
-            colors = (colors && colors.length >= 3) ? colors : [0.0, 0.0, 0.0];
-            this.colors = colors;
-            if (colors.length !== vertices.length) {
-                this.colors = this.colors.concat(fillColors(vertices.length / 3 - colors.length / 3, this.colors[0], this.colors[1], this.colors[2]));
-            }
-        }
-    };
-    
-    function surrogateConstructor() {};
-    surrogateConstructor.prototype = Shape.prototype;
-    var prototypeObject = new surrogateConstructor();
-    prototypeObject.constructor = sphere.constructor;
-    sphere.prototype = prototypeObject;
-    return sphere;
-    // return new Shape(sphereColors, vertices, sphereMode);
+var ShapeLibrary = { 
+    sphere: (
+        function () {
 
-})();
+            var sphere = function (n, colors, mode) {
+                var vertices = [];
+                for (var i = 0.0; i < n; i += 1) {
+                    for (var j = 0.0; j < n - 1; j += 1) {
+                        vertices = vertices.concat(
+                            Math.sin(Math.PI * i/n) * Math.cos(2 * Math.PI * j/n),
+                            Math.sin(Math.PI * i/n) * Math.sin(2 * Math.PI * j/n), 
+                            Math.cos(Math.PI * i/n)
+                        );
+                    }
+                }
+                Shape.call(this, colors, vertices, mode);
+            };
+
+            sphere.prototype = Object.create(Shape.prototype);
+            sphere.prototype.constructor = Shape;
+
+            return sphere;
+        }
+    )(),
+    square: (
+        function () {
+            // BLah...
+        }
+    )()
+};
