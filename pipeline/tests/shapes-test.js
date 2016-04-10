6dda9cc2fcee7c2b3f33b23e12e0c53721864efb
Frankie <fjames1@lion.lmu.edu>
2016-04-10 12:35:35 -0700

    I have started creating a good test suite for shapes... In doing so I am going to change the way my shapes work after a conversation with Dondi... I am going to try my best at sticking to TDD becuase it will save me time in the long run... So here are some basic creation tests to get me started...

--- pipeline/tests/shapes-test.js
+++ pipeline/tests/shapes-test.js
@@ -5,64 +5,94 @@ $(function () {
     // This suite checks instantiation basics.
     test("Creation and Data Access", function () {
         // Basic one color triangle...
-        var tri = new Shape({r: 1.0, g: 0.3, b: 0.6}, [].concat(
-                                                        [ 0.0, 0.0, 0.0 ],
-                                                        [ 0.5, 0.0, -0.75 ],
-                                                        [ 0.0, 0.5, 0.0 ]
-                                                       ), gl.TRIANGLES);
-        equal(tri.colors.length, tri.vertices.length, "Make sure there is a color for every vertices");
+        // A shape must be passed vertices and indices... the color can be defaulted...
+        // Additionally, the mode can default to LINES per say if not provided...
+        // Thus the following tests will test our creation and data accessing...
+
+        throws(
+            function () {
+                var fail2 = new Shape();
+            },
+            "Should throw exception for no provided indices or vertices"
+        );
+
+        throws (
+            function () {
+                var fail1 = new Shape([
+                                    [ 0.0, 0.0, 0.0 ],
+                                    [ 0.5, 0.0, -0.75 ],
+                                    [ 0.0, 0.5, 0.0 ]
+                                    ]
+                                );
+            },
+            "Should throw and exception for no provided indices"
+        );
+
+        // Should default to a mode of LINES and produce a triangle
+        // Color should default to black...
+        var triangle = new Shape([
+                                [ 0.0, 0.0, 0.0 ],
+                                [ 0.5, 0.0, -0.75 ],
+                                [ 0.0, 0.5, 0.0 ]
+                                ],
+                                [[0, 1, 2]]
+                            );
+        equal(triangle.colors.length, triangle.vertices.length, "Make sure there is a color for every vertices");
+        deepEqual([triangle.colors[0], triangle.colors[1], triangle.colors[2]], [0, 0, 0],
+            "Make sure that the default color is black");
+        equal(triangle.mode, 1, "Make sure the defualt mode is LINES (1)");
     });
 
-    test("Child creation and Data Access", function () {
-        var tri = new Shape({r: 1.0, g: 0.3, b: 0.6}, [].concat(
-                                                        [ 0.0, 0.0, 0.0 ],
-                                                        [ 0.5, 0.0, -0.75 ],
-                                                        [ 0.0, 0.5, 0.0 ]
-                                                       ), gl.TRIANGLES);
-        var triChild = tri.createChild();
-        equal(tri.children[0], triChild, "Make sure the child shape was added to children");
-
-    });
-
-    test("Functions of shapes (and their children)", function () {
-        // Translate child...
-        var tri = new Shape({r: 1.0, g: 0.3, b: 0.6}, [].concat(
-                                                        [ 0.0, 0.0, 0.0 ],
-                                                        [ 0.5, 0.0, -0.75 ],
-                                                        [ 0.0, 0.5, 0.0 ]
-                                                       ), gl.TRIANGLES);
-        var triChild = tri.createChild();
-
-        triChild.translate(0.5, 0.5, 0.5);
-        deepEqual(new Matrix().translate(0.5, 0.5, 0.5), triChild.matrix,
-                 "Make sure that child updates matrix on own");
-
-        var tri = new Shape({r: 1.0, g: 0.3, b: 0.6}, [].concat(
-                                                        [ 0.0, 0.0, 0.0 ],
-                                                        [ 0.5, 0.0, -0.75 ],
-                                                        [ 0.0, 0.5, 0.0 ]
-                                                       ), gl.TRIANGLES);
-        var triChild = tri.createChild();
-
-        tri.translate(0.5, 0.5, 0.5);
-        deepEqual(new Matrix().translate(0.5, 0.5, 0.5), triChild.matrix,
-                 "Make sure that parent updates childs matrix");
-
-        var tri = new Shape({r: 1.0, g: 0.3, b: 0.6}, [].concat(
-                                                        [ 0.0, 0.0, 0.0 ],
-                                                        [ 0.5, 0.0, -0.75 ],
-                                                        [ 0.0, 0.5, 0.0 ]
-                                                       ), gl.TRIANGLES);
-        var triChild = tri.createChild();
-        var triGrand = triChild.createChild();
-        tri.translate(0.5, 0.5, 0.5);
-        deepEqual(new Matrix().translate(0.5, 0.5, 0.5), triGrand.matrix,
-                 "Make sure that parent updates children's children matrix");
-
-        triChild.removeChild();
-
-        tri.translate(0,0,0);
-        deepEqual(new Matrix().translate(0.5, 0.5, 0.5), triGrand.matrix,
-                 "Make sure that parent stops updating once removed");        
-    });
+    // test("Child creation and Data Access", function () {
+    //     var tri = new Shape({r: 1.0, g: 0.3, b: 0.6}, [].concat(
+    //                                                     [ 0.0, 0.0, 0.0 ],
+    //                                                     [ 0.5, 0.0, -0.75 ],
+    //                                                     [ 0.0, 0.5, 0.0 ]
+    //                                                    ), gl.TRIANGLES);
+    //     var triChild = tri.createChild();
+    //     equal(tri.children[0], triChild, "Make sure the child shape was added to children");
+    //
+    // });
+    //
+    // test("Functions of shapes (and their children)", function () {
+    //     // Translate child...
+    //     var tri = new Shape({r: 1.0, g: 0.3, b: 0.6}, [].concat(
+    //                                                     [ 0.0, 0.0, 0.0 ],
+    //                                                     [ 0.5, 0.0, -0.75 ],
+    //                                                     [ 0.0, 0.5, 0.0 ]
+    //                                                    ), gl.TRIANGLES);
+    //     var triChild = tri.createChild();
+    //
+    //     triChild.translate(0.5, 0.5, 0.5);
+    //     deepEqual(new Matrix().translate(0.5, 0.5, 0.5), triChild.matrix,
+    //              "Make sure that child updates matrix on own");
+    //
+    //     var tri = new Shape({r: 1.0, g: 0.3, b: 0.6}, [].concat(
+    //                                                     [ 0.0, 0.0, 0.0 ],
+    //                                                     [ 0.5, 0.0, -0.75 ],
+    //                                                     [ 0.0, 0.5, 0.0 ]
+    //                                                    ), gl.TRIANGLES);
+    //     var triChild = tri.createChild();
+    //
+    //     tri.translate(0.5, 0.5, 0.5);
+    //     deepEqual(new Matrix().translate(0.5, 0.5, 0.5), triChild.matrix,
+    //              "Make sure that parent updates childs matrix");
+    //
+    //     var tri = new Shape({r: 1.0, g: 0.3, b: 0.6}, [].concat(
+    //                                                     [ 0.0, 0.0, 0.0 ],
+    //                                                     [ 0.5, 0.0, -0.75 ],
+    //                                                     [ 0.0, 0.5, 0.0 ]
+    //                                                    ), gl.TRIANGLES);
+    //     var triChild = tri.createChild();
+    //     var triGrand = triChild.createChild();
+    //     tri.translate(0.5, 0.5, 0.5);
+    //     deepEqual(new Matrix().translate(0.5, 0.5, 0.5), triGrand.matrix,
+    //              "Make sure that parent updates children's children matrix");
+    //
+    //     triChild.removeChild();
+    //
+    //     tri.translate(0,0,0);
+    //     deepEqual(new Matrix().translate(0.5, 0.5, 0.5), triGrand.matrix,
+    //              "Make sure that parent stops updating once removed");
+    // });
 });
