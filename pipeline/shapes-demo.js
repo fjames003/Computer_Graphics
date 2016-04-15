d8ffeaff5c156eafe6b5973885ac937f15943483
Frankie James <fjames1@lion.lmu.edu>
2016-04-15 15:43:47 -0700

    Changed up the scene a little bit and found a bug... If I split enough, I start to overlap spheres and don't appear to the user... Will have to work on how to take care of this...

--- pipeline/shapes-demo.js
+++ pipeline/shapes-demo.js
@@ -31,10 +31,17 @@
     gl.viewport(0, 0, canvas.width, canvas.height);
 
     // Build the objects to display.
-    const aSphere =  new Sphere (25, gl.TRIANGLES, { r: 1.0, g: 0.5, b: 0.0 }).translate(-1, -1, -10);
+    const aSphere =  new Sphere (25, gl.TRIANGLES, { r: 1.0, g: 0.5, b: 0.0 }).translate(0, -1, -10);
     // const aSphereKid = aSphere.createChild().translate(0, 1.5, 5).scale(0.5, 0.5, 0.5);
-    const cube = aSphere.createChild(new Cube(gl.TRIANGLES, { r: 0.5, g: 0.5, b: 0.5 })).translate(3, 3, 0).scale(0.5, 0.5, 0.5);
+    // const cube = aSphere.createChild(new Cube(gl.TRIANGLES, { r: 0.5, g: 0.5, b: 0.5 })).translate(3, 3, 0).scale(0.5, 0.5, 0.5);
+
+    // 2 Spheres...
+    aSphere.split(1, 'x');
+    // 4 Spheres
+    aSphere.split(2, 'y');
+    // 8 Spheres
     aSphere.split(2, 'x');
+    // 16 Spheres
     aSphere.split(2, 'y');
 
     const objectsToDraw = [
